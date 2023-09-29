72be4e5bf7bc9fd9e995d0643c743ffc63ecfcbb
Swarda-Khatal <swarda.khatal@gmail.com>
2023-09-29 13:41:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/photopicker/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.photopicker
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.photopicker.ui.theme.PhotoPickerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PhotoPickerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    PhotoPickerDemo()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PhotoPickerDemo() {
+    var selectedImage by rememberSaveable(inputs = emptyArray()) {
+        mutableStateOf<Uri?>(null)
+    }
+    var selectedImages by rememberSaveable(inputs = emptyArray()) {
+        mutableStateOf<List<Uri>>(emptyList())
+    }
+
+    val singleImageSelectionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri ->
+            selectedImage = uri
+        }
+    )
+
+    val multipleImageSelectionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickMultipleVisualMedia(),
+        onResult = { uris ->
+            selectedImages = uris
+        }
+    )
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Button(onClick = {
+                    singleImageSelectionLauncher.launch(PickVisualMediaRequest(
+                        ActivityResultContracts.PickVisualMedia.ImageOnly
+                    ))
+                }) {
+                    Text(text = "Single Image")
+                }
+                Button(onClick = {
+                    multipleImageSelectionLauncher.launch(PickVisualMediaRequest(
+                        ActivityResultContracts.PickVisualMedia.ImageOnly))
+                }) {
+                    Text(text = "Multiple Images")
+                }
+            }
+        }
+        item {
+            AsyncImage(
+                model = selectedImage,
+                contentDescription = "",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+        }
+        items(selectedImages) { Uri ->
+            AsyncImage(
+                model = Uri,
+                contentDescription = "",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    PhotoPickerTheme {
+        PhotoPickerDemo()
+    }
+}
